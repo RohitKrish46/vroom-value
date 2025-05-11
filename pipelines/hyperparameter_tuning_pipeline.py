69d09ba1ea6646ff67c9b6f8920f06e00facd5c2
Rohit Krishnan <krishr.somu@gmail.com>
2025-05-11 08:15:10 +0530

    refactored hyperparameter tuning pipeline.

--- pipelines/hyperparameter_tuning_pipeline.py
+++ pipelines/hyperparameter_tuning_pipeline.py
@@ -1,54 +1,37 @@
 from zenml import pipeline
 from typing import List, Tuple
-from steps.model_selection_step import select_top_models_step
+from steps.model_evaluator_step import model_evaluator_step
 from steps.hyperparameter_tuning_step import tune_models_step
-from steps.handle_missing_values_step import handle_missing_values_step
-from steps.feature_engineering_step import feature_engineering_step
-from steps.outlier_detection_step import outlier_detection_step
-from steps.data_splitter_step import data_splitter_step
 from steps.retrain_best_model_step import retrain_best_model_step
-from steps.data_ingestion_step import data_ingestion_step   # Assuming you already have it
+from steps.data_loader_step import load_data  
 
 @pipeline(
     enable_cache=False,
     name="prices_predictor")
-def hyperparameter_tuning_pipeline(top_run_ids: List[str]) -> Tuple[str, dict]:
+def hyperparameter_tuning_pipeline(top_run_ids: List[str], data_engineering_run_id: str) -> Tuple[str, dict]:
     """Pipeline to select top models and tune hyperparameters."""
-
-    # Step 1: Data ingestion
-    raw_data = data_ingestion_step(
-        file_path="D:/Repositories/vroom-value-with-pipelines/data/Archive.zip"
-    )
-
-    # Step 2: Handle missing values
-    cleaned_data = handle_missing_values_step(raw_data)
-
-    # Step 3: Feature engineering
-    engineered_data = feature_engineering_step(
-        cleaned_data, strategy="log", features=["selling_price", "km_driven"]
-    )
-
-    # Step 4: Outlier detection
-    clean_data = outlier_detection_step(engineered_data)
-
-    # Step 5: Data splitting
-    X_train, X_test, y_train, y_test = data_splitter_step(
-        clean_data, target_column="selling_price"
-    )
-    # Step 6: Hyperparameter tuning
+    
+    # Step 1: load data from the artifact store
+    X_train, X_test, y_train, y_test = load_data(data_engineering_run_id)
+    
+    # Step 1: Hyperparameter tuning
     best_run_id, best_params = tune_models_step(
         top_run_ids=top_run_ids, 
         X_train=X_train, 
-        X_test=X_test, 
         y_train=y_train, 
-        y_test=y_test
         )
-    # Step 7: Retrain the best model
-    retrain_best_model_step(
+    # Step 2: Retrain the best model
+    trained_model, cur_run_id = retrain_best_model_step(
         best_run_id=best_run_id, 
         best_params=best_params, 
         X_train=X_train, 
         y_train=y_train
     )
+    # Step 3: Evaluate the retrained model
+    r2, mse = model_evaluator_step(
+        trained_model=trained_model,
+        X_test=X_test,
+        y_test=y_test
+    )
     
-    return best_run_id, best_params
+    return best_run_id, best_params, cur_run_id
